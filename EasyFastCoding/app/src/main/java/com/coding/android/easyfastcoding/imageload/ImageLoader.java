62644535e55ecce4ef02c188848f72169f451be1
chengyang <yang.cheng@choumei.cn>
2015-11-12 14:21:08 +0800

    加入glide全局配置

--- EasyFastCoding/app/src/main/java/com/coding/android/easyfastcoding/imageload/ImageLoader.java
+++ EasyFastCoding/app/src/main/java/com/coding/android/easyfastcoding/imageload/ImageLoader.java
@@ -1,10 +1,16 @@
 package com.coding.android.easyfastcoding.imageload;
 
+import android.content.Context;
+import android.widget.ImageView;
+
 /**
  * Created by cy on 2015/11/10.
  */
 public interface ImageLoader {
 
-    public void display(String url, int imgId);
+    public void didplay(Context context, String url, ImageView imageView);
+
+    public void display(Context context, String url, ImageView imageView, int holderResourceId);
+
 
 }
--- EasyFastCoding/app/src/main/java/com/coding/android/easyfastcoding/imageload/glide/common/GlideImageLoader.java
+++ EasyFastCoding/app/src/main/java/com/coding/android/easyfastcoding/imageload/glide/common/GlideImageLoader.java
@@ -1,14 +1,22 @@
 package com.coding.android.easyfastcoding.imageload.glide.common;
 
-import com.coding.android.easyfastcoding.imageload.ImageLoader;
+import android.content.Context;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 /**
  * Created by cy on 2015/11/10.
  */
-public class GlideImageLoader implements ImageLoader {
+public class GlideImageLoader {
 
-    @Override
-    public void display(String url, int imgId) {
-
+    public static void didplay(Context context, String url, ImageView imageView) {
+        Glide.with(context).load(url).centerCrop().into(imageView);
     }
+
+    public static void display(Context context, String url, ImageView imageView, int holderResourceId) {
+        Glide.with(context).load(url).placeholder(holderResourceId).centerCrop().into(imageView);
+    }
+
+
 }
